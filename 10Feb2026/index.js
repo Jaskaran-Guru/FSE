635fdc02a5e72e25dbf98639508bd8babbb17ea9
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2026-02-10 10:12:35 +0530

    10feb2026

--- 10Feb2026/index.js
+++ 10Feb2026/index.js
@@ -5,6 +5,17 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello, World!');
 });
 
+app.get('/api/data', (req, res) => {
+    const data = {
+        message: 'This is some sample data.',
+        timestamp: new Date()
+    };
+    res.status(200).json(data);
+}
+);
+
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
